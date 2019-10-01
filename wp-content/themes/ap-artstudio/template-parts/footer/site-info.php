765e490142713c270fa6fc4f8352a9350f624edd
Aimy Palogiannidou <emmapalo@gmail.com>
2019-10-01 12:19:06 +0100

    footer date changed

--- wp-content/themes/ap-artstudio/template-parts/footer/site-info.php
+++ wp-content/themes/ap-artstudio/template-parts/footer/site-info.php
@@ -10,7 +10,7 @@
 
 ?>
 <div class="site-info">
-	<span>Copyright &copy; <?php date('Y'); ?> <?php echo get_bloginfo('name'); ?></span><br>
+	<span>Copyright &copy; <?php echo date('Y'); ?> <?php echo get_bloginfo('name'); ?></span><br>
 	<a href="<?php echo esc_url( __( 'https://#/', 'twentyseventeen' ) ); ?>" class="imprint">
 		<?php printf( __( 'Web design by %s', 'twentyseventeen' ), 'AP Art Studio' ); ?>
 	</a>
